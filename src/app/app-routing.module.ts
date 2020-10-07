0f6b8f8bd772604c8141da00add7dc9751e080bd
Taylor McPherson <taylormcpherson@Taylors-MacBook-Air.local>
2020-10-07 08:53:06 -0700

    updating resume?

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from "./about/about.component";
-import { WorkComponentComponent } from "./work/work-component.component";
-import { ResumeComponent } from "./resume/resume.component";
-import {CriticalDesignComponent} from "./case-study/critical-design/critical-design.component";
-import {GroupSchedulingComponent} from "./case-study/group-scheduling/group-scheduling.component";
-import {ApprenticeshipLearningComponent} from "./case-study/apprenticeship-learning/apprenticeship-learning.component";
-import {PlaygroundComponent} from "./case-study/playground/playground.component";
+import { AboutComponent } from './about/about.component';
+import { WorkComponentComponent } from './work/work-component.component';
+import { ResumeComponent } from './resume/resume.component';
+import { CriticalDesignComponent } from './case-study/critical-design/critical-design.component';
+import { GroupSchedulingComponent } from './case-study/group-scheduling/group-scheduling.component';
+import { ApprenticeshipLearningComponent } from './case-study/apprenticeship-learning/apprenticeship-learning.component';
+import { PlaygroundComponent } from './case-study/playground/playground.component';
 
 
 
